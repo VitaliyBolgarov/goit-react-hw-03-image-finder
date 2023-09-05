e3b1ae508c83b3ac3fa1b96c06c5454b56b0bd97
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-09-05 18:24:06 +0300

    v1.3

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -2,8 +2,7 @@ export const PER_PAGE = 12;
 
 export async function fetchImages(page, query) {
   const API_KEY = '38442496-05931c79f09bcb64844571ca2';
-  const BASE_URL = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}
-  &image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`;
+  const BASE_URL = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`;
   const response = await fetch(BASE_URL);
   return response.ok
     ? response.json()
